e1511ffba4d71723100c71aee461908e8cec9d50
KentoNishi <kento24gs@outlook.com>
2021-02-05 17:54:41 -0800

    App layout

---
+++ src/components/MainView.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app>
+    <v-app-bar dark color="blue" app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{ APPNAME }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-switch
+        class="navSwitch"
+        v-model="$vuetify.theme.dark"
+        color="white"
+        label="Dark Theme"
+      ></v-switch>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card color="#385F73" dark class="card">
+            <v-card-title class="headline"
+              >Welcome to LHS WebDev's PWA Demo</v-card-title
+            >
+
+            <v-card-subtitle>More content will be added soon.</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text href="http://lhswebdev.github.io/" target="_blank"
+                >Visit Website</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    APPNAME: "WebDev PWA"
+  }),
+  created() {
+    document.title = this.APPNAME;
+  }
+};
+</script>
+
+<style>
+.card {
+  --margin: 10px;
+  width: calc(100% - 2 * var(--margin));
+  margin: var(--margin) auto var(--margin) auto;
+}
+
+.navSwitch {
+  transform: translateY(25%);
+}
+
+html {
+  overflow-y: auto !important;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MainView msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainView from "@/components/MainView.vue";
+
+export default {
+  name: "Home",
+  components: {
+    MainView
+  }
+};
+</script>
