17707d0d8cbcbd7cebadbf78376ec5a132703c58
KentoNishi <KentoNishi@users.noreply.github.com>
2021-02-08 16:30:51 +0000

    deploy: 6097b2ce1bd9cd4a65f900fb8916724a4f464c39

---
+++ precache-manifest.beeed1bfa84efab4bc499260ad0716be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bc2dc2e278a2b890ab",
+    "url": "/vuetify-pwa/css/app.cb050b55.css"
+  },
+  {
+    "revision": "ae9b8228de81ad2e22f8",
+    "url": "/vuetify-pwa/css/chunk-vendors.3478fc23.css"
+  },
+  {
+    "revision": "182642b62565f18bddac9bd3d9b62eb8",
+    "url": "/vuetify-pwa/index.html"
+  },
+  {
+    "revision": "57bc2dc2e278a2b890ab",
+    "url": "/vuetify-pwa/js/app.49e197cc.js"
+  },
+  {
+    "revision": "ae9b8228de81ad2e22f8",
+    "url": "/vuetify-pwa/js/chunk-vendors.a9390020.js"
+  },
+  {
+    "revision": "3f2f9de7f3147b660e71cbdc0c6bb288",
+    "url": "/vuetify-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetify-pwa/robots.txt"
+  }
+]);
